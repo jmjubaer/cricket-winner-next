393613cddc0f82ed9f28dfc77bdaf69831ec7a00
jmjubaer <jmjubaer3927@gmail.com>
2023-11-25 15:41:51 +0600

    more update in artcle tap

--- src/Page/Home/MainContent/TopContent.js
+++ src/Page/Home/MainContent/TopContent.js
@@ -147,6 +147,83 @@ const TopContent = () => {
                             ))}
                         </div>
                     </div>
+                    {/*=============== Cricket Analysis ================= */}
+                    <div className="">
+                        <h2 className="text-2xl font-medium">Cricket Analysis</h2>
+                        <div className="">
+                            {newsCollection.slice(0,2).map((news, idx) => (
+                                <div
+                                    key={idx}
+                                    className="flex mt-[10px] py-[10px] gap-5 "
+                                >
+                                    <Image
+                                        width={245}
+                                        height={168}
+                                        alt="image Image"
+                                        className="w-[245px] h-[170px] object-cover rounded-xl"
+                                        src={news?.image}
+                                    />
+
+                                    <div className="">
+                                        <div className="flex items-center gap-5">
+                                            <p className="text-[#969696]">
+                                                <FaRegClock className="inline mr-2" />
+                                                {moment(
+                                                    news?.timestamp
+                                                ).fromNow()}
+                                            </p>
+                                            <p className="px-3 py-[5px] bg-[#323232] rounded-3xl w-fit font-medium text-white text-sm">
+                                                {"Asia Cup"}
+                                            </p>
+                                        </div>
+                                        <h3 className="font-bold text-xl mt-[10px]">
+                                            {news?.title}
+                                        </h3>
+                                        <p className="mt-[10px]">{news?.description}</p>            
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                    {/*=============== Cricket Stats ================= */}
+                    <div className="">
+                        <h2 className="text-2xl font-medium">Cricket Stats</h2>
+                        <div className="">
+                            {newsCollection.slice(0,2).map((news, idx) => (
+                                <div
+                                    key={idx}
+                                    className="flex mt-[10px] py-[10px] gap-5 "
+                                >
+                                    <Image
+                                        width={245}
+                                        height={168}
+                                        alt="image Image"
+                                        className="w-[245px] h-[170px] object-cover rounded-xl"
+                                        src={news?.image}
+                                    />
+
+                                    <div className="">
+                                        <div className="flex items-center gap-5">
+                                            <p className="text-[#969696]">
+                                                <FaRegClock className="inline mr-2" />
+                                                {moment(
+                                                    news?.timestamp
+                                                ).fromNow()}
+                                            </p>
+                                            <p className="px-3 py-[5px] bg-[#323232] rounded-3xl w-fit font-medium text-white text-sm">
+                                                {"Asia Cup"}
+                                            </p>
+                                        </div>
+                                        <h3 className="font-bold text-xl mt-[10px]">
+                                            {news?.title}
+                                        </h3>
+                                        <p className="mt-[10px]">{news?.description}</p>            
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </TabPanel>
 
                 {/*================= Prediction Tab -=================== */}
