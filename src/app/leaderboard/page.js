6722a0648b2efc0bcf470023ae5ebd1dddd49dac
jmjubaer <jmjubaer3927@gmail.com>
2023-12-08 15:53:44 +0600

    50%

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -1,9 +1,12 @@
 "use client";
 import VectorBg from "@/components/VectorBg";
+import ProgressBar from "@ramonak/react-progress-bar";
 import Image from "next/image";
 import React, { useState } from "react";
+import { AiFillDislike, AiFillLike } from "react-icons/ai";
 import { FaUserAlt } from "react-icons/fa";
 import { FaUserCheck } from "react-icons/fa6";
+import { PiWarningCircleBold } from "react-icons/pi";
 import { SlBadge } from "react-icons/sl";
 import Select from "react-select";
 const LeaderBoardPage = () => {
@@ -14,93 +17,198 @@ const LeaderBoardPage = () => {
         {
             name: "Phoenix Baker",
             image: "https://i.ibb.co/kKtSzMP/my-image.gif",
-            win: 10,
-            loss: 5,
             accuracy: 25,
+            win: {
+                toss: 5,
+                match: 6,
+            },
+            loss: {
+                toss: 1,
+                match: 0,
+            },
+            participation: 6,
+            badge: 5,
         },
         {
             name: "Olivia Rhye",
             image: "https://i.ibb.co/ZBHHzfC/istockphoto-1224641458-612x612.jpg",
-            win: 30,
-            loss: 15,
+            win: {
+                toss: 5,
+                match: 8,
+            },
+            loss: {
+                toss: 4,
+                match: 1,
+            },
+            participation: 9,
             accuracy: 80,
+            badge: 0,
         },
         {
             name: "Phoenix Baker",
             image: "https://i.ibb.co/VCzddGZ/login-bg.jpg",
-            win: 45,
-            loss: 10,
+            win: {
+                toss: 7,
+                match: 2,
+            },
+            loss: {
+                toss: 10,
+                match: 15,
+            },
+            participation: 17,
             accuracy: 55,
+            badge: 0,
         },
         {
             name: "Olivia Rhye",
             image: "https://i.ibb.co/1M9VFxq/shiful.gif",
-            win: 50,
-            loss: 8,
+            win: {
+                toss: 12,
+                match: 5,
+            },
+            loss: {
+                toss: 5,
+                match: 12,
+            },
+            participation: 17,
             accuracy: 75,
+
+            badge: 2,
         },
         {
             name: "Phoenix Baker",
             image: "https://i.ibb.co/z43WCJV/banner-1.jpg",
-            win: 85,
-            loss: 50,
+            win: {
+                toss: 23,
+                match: 15,
+            },
+            loss: {
+                toss: 2,
+                match: 10,
+            },
+            participation: 25,
             accuracy: 45,
+            badge: 0,
         },
         {
             name: "Olivia Rhye",
             image: "https://i.ibb.co/hK7QMCM/chef-6.jpg",
-            win: 80,
-            loss: 50,
+            win: {
+                toss: 6,
+                match: 10,
+            },
+            loss: {
+                toss: 10,
+                match: 6,
+            },
+            participation: 16,
             accuracy: 15,
+            badge: 5,
         },
         {
             name: "Phoenix Baker",
             image: "https://i.ibb.co/ck3p02f/chef-4.jpg",
-            win: 75,
-            loss: 12,
+            win: {
+                toss: 4,
+                match: 10,
+            },
+            loss: {
+                toss: 9,
+                match: 3,
+            },
+            participation: 13,
             accuracy: 45,
+            badge: 1,
         },
         {
             name: "Olivia Rhye",
             image: "https://i.ibb.co/kKtSzMP/my-image.gif",
-            win: 10,
-            loss: 5,
+            win: {
+                toss: 5,
+                match: 9,
+            },
+            loss: {
+                toss: 10,
+                match: 6,
+            },
+            participation: 15,
             accuracy: 25,
+            badge: 3,
         },
         {
             name: "Phoenix Baker",
             image: "https://media.istockphoto.com/id/1485546774/photo/bald-man-smiling-at-camera-standing-with-arms-crossed.jpg?s=612x612&w=0&k=20&c=9vuq6HxeSZfhZ7Jit_2HPVLyoajffb7h_SbWssh_bME=",
-            win: 40,
-            loss: 15,
+            win: {
+                toss: 1,
+                match: 3,
+            },
+            loss: {
+                toss: 5,
+                match: 4,
+            },
+            participation: 6,
             accuracy: 65,
+            badge: 0,
         },
         {
             name: "Jack Leo",
             image: "https://media.istockphoto.com/id/1435745704/photo/portrait-of-smiling-mid-adult-businessman-standing-at-corporate-office.jpg?s=612x612&w=0&k=20&c=NtTebZxpAfw964hJJut8WFa__eZEfO07CAKdqeFBrFU=",
-            win: 80,
-            loss: 51,
+            win: {
+                toss: 0,
+                match: 1,
+            },
+            loss: {
+                toss: 1,
+                match: 0,
+            },
+            participation: 1,
             accuracy: 75,
+            badge: 0,
         },
         {
             name: "Mia Kelly",
             image: "https://media.istockphoto.com/id/1473323104/photo/handsome-mid-adult-man-dressed-in-denim-shirt-screaming-and-cheerfully-pumping-fist-while.jpg?s=612x612&w=0&k=20&c=KUPCm3h2v3lgJbuRQFIWWDg95Fwkgjsta5OzmmscZHE=",
-            win: 90,
-            loss: 15,
+            win: {
+                toss: 5,
+                match: 10,
+            },
+            loss: {
+                toss: 5,
+                match: 0,
+            },
+            participation: 10,
             accuracy: 85,
+            badge: 1,
         },
         {
             name: "Leo Kelly",
             image: "https://media.istockphoto.com/id/1437931505/photo/businessman-digital-tablet-or-strategy-planning-in-hotel-conference-lobby-or-airport-travel.jpg?s=612x612&w=0&k=20&c=OGRd22Usakx9wHTQsKto0qagIlH38sWDPEmmmXcIBSQ=",
-            win: 70,
-            loss: 50,
+            win: {
+                toss: 5,
+                match: 7,
+            },
+            loss: {
+                toss: 9,
+                match: 7,
+            },
+            participation: 14,
             accuracy: 55,
+            badge: 2,
         },
         {
             name: "Mia Phoenix",
             image: "https://i.ibb.co/kKtSzMP/my-image.gif",
-            win: 10,
-            loss: 5,
+            win: {
+                toss: 0,
+                match: 10,
+            },
+            loss: {
+                toss: 1,
+                match: 5,
+            },
+            participation: 15,
             accuracy: 25,
+            badge: 0,
         },
     ];
     const YearOptions = [
@@ -185,7 +293,7 @@ const LeaderBoardPage = () => {
                 </div>
             </VectorBg>
             {/* Main content */}
-            <div className="grid grid-cols-3 mt-5 shadow-md">
+            <div className="grid grid-cols-3 mt-5 gap-5">
                 {/* Left Content */}
                 <div className="bg-white p-5 rounded-xl">
                     <div className="flex justify-between items-center border-b pb-4">
@@ -229,7 +337,82 @@ const LeaderBoardPage = () => {
                     </div>
                 </div>
                 {/* Right Content */}
-                <div className="col-span-2"></div>
+                <div className="col-span-2">
+                    <div className="overflow-auto w-full">
+                        <table className=" w-full">
+                            {/* head */}
+                            <thead>
+                                <tr className="font-medium my-3 text-[#969696] bg-[#EAECF0]  text-sm">
+                                    <th className="font-medium py-3 px-2">
+                                        Rank
+                                    </th>
+                                    <th className="font-medium py-3 text-left px-2">
+                                        User Average Accuracy
+                                    </th>
+                                    <th className="font-medium py-3 px-2">
+                                        Toss
+                                    </th>
+                                    <th className="font-medium py-3 px-2">
+                                        Match
+                                    </th>
+                                    <th className="font-medium py-3 px-2">
+                                    Participation
+                                    </th>
+                                    <th className="font-medium py-3 px-2">
+                                    Badge
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/*Todo: Change Dynamic data */}
+                                {usersInfo?.slice(0, 10)?.map((user, idx) => (
+                                    <tr key={idx} className="even:bg-[#EAECF0]">
+                                        <th>{idx + 1}</th>
+                                        <td className="py-2">
+                                            <div className="flex justify-between sm:gap-2">
+                                                <div className="flex items-center gap-1">
+                                                    <Image
+                                                        width={30}
+                                                        height={30}
+                                                        className="w-[30px] h-[30px] object-cover rounded-full border"
+                                                        src={user?.image}
+                                                        alt="User Image"
+                                                    />
+                                                    <p className="font-medium text-sm">
+                                                        {user?.name}
+                                                    </p>
+                                                </div>
+                                                <p
+                                                    className={`bg-opacity-10 w-fit sm:text-sm text-xs leading-3 border flex justify-center items-center rounded-full text-center py-1 px-[6px] font-medium ${
+                                                        user?.accuracy > 40
+                                                            ? "bg-[#027A48] text-[#027A48]"
+                                                            : user?.accuracy >
+                                                              30
+                                                            ? "bg-[#FF8B00] text-[#FF8B00]"
+                                                            : "bg-[#E33E38] text-[#E33E38]"
+                                                    }`}
+                                                >
+                                                    {user?.accuracy}%
+                                                </p>
+                                            </div>
+                                            <ProgressBar
+                                                className="mt-[10px]"
+                                                isLabelVisible={false}
+                                                bgColor="#FFC305"
+                                                completed={user?.accuracy}
+                                                height="12px"
+                                            />
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </section>
     );
